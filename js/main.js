b9e7f69973197a9ba0c3884923e7b5db6e753646
IbrahimIrfan <1ibrahimirfan@gmail.com>
2017-05-28 16:28:06 -0400

    Factored out JS and achanged HTML structure

--- js/main.js
+++ js/main.js
@@ -1,146 +1,111 @@
 var projects = [{
         "id": 0,
         "title": "Point-To-Speech",
-        "width": 640,
         "description": "MasseyHacks 3 - 3rd Place Finalist.<br><br>Created a device to help blind people read regular pages of text using simple, intuitive hand gestures. A Raspberry Pi with a connected camera was attached to PVC pipe. The paper was placed below the camera, with a LeapMotion attached with a 3D printed stand in front of it.<br><br> Once the user places the paper in position, the camera takes a picture and sends it to a server. Then, the accompanying web application uses <a href='http://tesseract.projectnaptha.com/' target='_blank'>TesseractJS</a> to parse the text.<br><br> Using calculated offset and conversion factors, the coordinates returned by the program were mapped for the LeapMotion - so that the word had a location in physical space.<br><br> Then, when the user points to a word on the page, the web app will say the word using <a href='https://responsivevoice.org/' target='_blank'>ResponsiveVoiceJS</a>. ",
-        "thumbnail": "MH1.jpg",
         "images": ['MH1.jpg', 'MH2.jpg', 'MH3.jpg', 'MH4.jpg'],
         "languages": ['HTML', 'CSS', 'JS', 'PHP', 'Python', 'Raspberry Pi', 'Leap Motion', 'jQuery', 'node.js', 'AJAX']
     },
     {
         "id": 1,
         "title": "DECA Web Application",
-        "width": 640,
         "description": "Last year, my school's performance in DECA, a student business competition, was disappointing. Overall scores for competitors are determined by an interview and multiple-choice exam; we did great on the interviews, but lacked on the exams. <br><br> Since the exams that DECA releases are not in a user-friendly format, it was a hassle to practice, and it hindered our success. Not only that, our club was doing registration for over 200 members by hand.<br><br>Thus, I decided to completely redo our existing website, creating a program would read the practice exams that DECA releases and output self-marking online forms. I then implemented a registration and login system for members to take those exams under their name on our website; allowing the executives to monitor their progress.<br><br> Other functionality included executive-only announcement posting (along with email notifications once an announcement is posted), as well as other resources for each event.",
-        "thumbnail": "DECA.png",
         "images": ['DECA2.jpg', 'DECA3.jpg', 'DECA4.jpg', 'DECA5.jpg', 'DECA6.jpg', 'DECA7.jpg', 'DECA8.jpg', 'DECA9.jpg'],
         "languages": ['HTML', 'CSS', 'JS', 'PHP', 'SQL', 'jQuery', 'node.js', 'AJAX']
     },
     {
         "id": 2,
         "title": "Halcyon",
-        "width": 640,
         "description": "A non profit mental health organization called Halcyon to improve wait times for mental health patients using mobile apps.<br><br>Myself and 12 other friends from the SHAD Valley Program have been working for the past few months do develop a solution which includes the following: <br><br>1) Self care plan<br>2) Personalized specialists and emergency department suggestions<br>3) Emergency hotlines<br>4) Anonymous group support sorum<br>5) Treatment and medication information",
-        "thumbnail": "halcyon.jpg",
         "images": ['halcyon.jpg'],
         "languages": ['HTML/CSS/JS/PHP - Web Client', 'Swift 3 - iOS', 'Java - Android']
     },
     {
         "id": 3,
         "title": "University of Guelph Exam Network",
-        "width": 640,
         "description": "During my internship as a Software Developer for Viceroy Media, I worked on an exam network web application for the University of Guelph.<br><br>It contained a file upload system where UofG students could upload study material for specific courses for others to use. The application also included a login system with Facebook API integration, a public forum, and course/professor ratings.<br><br> My specific role was to develop the functionality for the file system (uploading, managing, and viewing the uploaded material) and login system - working with JS/jQuery and API requests to do so.",
-        "thumbnail": "uog.jpg",
         "images": ['uog.jpg', 'uog2.jpg', 'uog3.jpg', 'uog4.jpg', 'uog5.jpg'],
         "languages": ['HTML', 'CSS', 'JS', 'SQL', 'jQuery', 'node.js', 'AJAX', 'Go']
     },
     {
         "id": 4,
         "title": "BridgeDay Conference Website",
-        "width": 640,
         "description": "At my high school, a full day conference is hosted every year where incoming students come to the shool for a day of talks, workshops, and introductions.<br><br>When 350 grade 8 students are coming to your school from many different locations, you need someone to take on the hectic registration process - that was my role.<br><br>I decided to make a conference website, showcasing the plan of the day and with important information for students and teacher supervisors. They were also able to register directly from the website - which allowed us to send various schools a simple link instead of a long google form.<br><br>Students had a choice of what workshops they wished to attend, with their preference indicated on the registration form. As workshops were filled on a first come first serve basis, I implemented a live \"Workshop Status\" display, indicating the spaces currently available in each workshop. Along with this, I created a program which would generate nametags for every student attending the conference.<br><br> Overall, this was a great opportunity for me to experience how to properly manage a registration system and how to correctly deal with data. In addition - the conference turned out very successful, with no major hiccups.",
-        "thumbnail": "bridgeday.jpg",
         "images": ['bridgeday.jpg', 'bridgeday2.jpg', 'bridgeday3.jpg', 'bridgeday4.jpg', 'nametags.jpg'],
         "languages": ['HTML', 'CSS', 'JS', 'PHP', 'SQL', 'jQuery', 'AJAX']
     },
     {
         "id": 5,
         "title": "Balls iOS Game",
-        "width": 640,
         "description": "A new obsessive trend spread across my school - <a href='https://itunes.apple.com/us/app/ballz/id1139609950?mt=8' target='_blank'>Ballz</a>. With a 4 day weekend approaching, I decided to try and recreate it. <br><br> The final product turned out much better than I expected - the game was fully functional with a single ball. In the above picture, my version is on the left and the real game is on the right.",
-        "thumbnail": "balls.jpg",
         "images": ['balls.jpg', 'balls2.png'],
         "languages": ['Swift 3']
     },
     {
         "id": 6,
         "title": "TU20 Website",
-        "width": 640,
         "description": "As the Web Developer for TechUnderTwenty - a non-profit youth organization, I have been developing a new website for us as our current website is made with a template. <br><br>Expected launch: June 2017",
-        "thumbnail": "tu20.jpg",
         "images": ['tu20.jpg'],
         "languages": ['HTML', 'CSS', 'JS', 'SQL', 'jQuery']
     },
     {
         "id": 7,
         "title": "Tragic Heroes iOS Game",
-        "width": 640,
         "description": "For a creative project in my English class, I chose to create a game to explain the concept of tragic heroes. <br><br><iframe width='560' height='315' src='https://www.youtube.com/embed/T9b6KoyhZhQ?rel=0&amp;showinfo=0' frameborder='0' allowfullscreen></iframe>",
-        "thumbnail": "tragic.jpg",
         "images": [],
         "languages": ['Swift 3']
     },
     {
         "id": 8,
         "title": "IRHS Announcements Page",
-        "width": 640,
         "description": "As my school's <a href='http://irs.hdsb.ca/announcements/' target='_blank'>announcements page</a> is ugly (second picture above), I decided to create an alternate announcements page for our school. I used a cross-origin GET request with AJAX and then parsed the response to create a sligtly more aesthetically pleasing version of our announcements page.",
-        "thumbnail": "irhsann.jpg",
         "images": ['irhsann.jpg', 'irhsann2.jpg'],
         "languages": ['HTML', 'CSS', 'JS', 'jQuery', 'AJAX']
     },
     {
         "id": 9,
         "title": "Pinch Perfect",
-        "width": 640,
         "description": "As a HackWestern 3 project, this instrument requires no physical contact to play. The LeapMotion tracks your hand movements and plays a note accordingly. Using your right hand, you control the frequency, while your left-hand controls volume.<br><br> To play, all you need to do is pinch your fingers on your right hand. The x-axis controls pitch modulation to produce a vibrato, so by shaking your right hand side to side, you can create a sound that mimics the human voice.<br><br> This hands-free system has a game mode and free play mode. In game mode, you can follow the purple bar depicted on the screen to play some of your favorite songs, whereas in free play, you are free to create and practice. You can also create and upload your own songs for others to play and enjoy. <br><br>It is great for people who cannot play an instrument due to disorders like arthritis or Parkinsons. Additionally, it is helpful for developing fine motor skills, as a therapeutic medium, or to help with ear training, and can be enjoyed by people of all ages.",
-        "thumbnail": "pinch.jpg",
         "images": ['pinch.jpg', 'pinch2.jpg', 'pinch3.jpg', 'pinch4.jpg'],
         "languages": ['HTML', 'CSS', 'JS', 'PHP', 'SQL', 'jQuery', 'AJAX']
     },
     {
         "id": 10,
         "title": "Running Goat iOS Game",
-        "width": 640,
         "description": "My first iOS App - a simple goat game. Avoid the walls by tapping on the screen to flip the goat. <br><br><a href='https://www.youtube.com/watch?v=hLOhirsXx9c' target='_blank'>Tutorial followed</a>",
-        "thumbnail": "goat.jpg",
         "images": ['goat.jpg', 'goat2.jpg', 'goat3.jpg'],
         "languages": ['Swift 3']
     },
     {
         "id": 11,
         "title": "Turing Chess Game",
-        "width": 640,
         "description": "My final project for Grade 11 Computer Science - a two-player Chess game built with Turing. One of the most difficult yet satisfying projects I have taken on. The final product was nearly fully functional - all but checkmate detection was working.",
-        "thumbnail": "chess.jpg",
         "images": ['chess.jpg', 'chess2.jpg', 'chess3.jpg', 'chess4.jpg'],
         "languages": ['Turing']
     },
     {
         "id": 12,
         "title": "Wheel of Python",
-        "width": 640,
         "description": "Project for Grade 12 Computer Science. Created a wheel spinning program using Python Tkinter to simulate the spin of a wheel.",
-        "thumbnail": "wheelofpython.jpg",
         "images": ['wheelofpython.jpg'],
         "languages": ['Python']
     },
     {
         "id": 13,
         "title": "EnTech IRHS Proposal Website",
-        "width": 640,
         "description": "Created a supplementary website for my school's <a href='http://www.oce-ontario.org/programs/commercialization-programs/advancingeducation-program' target='_blank'>OCE Advancing Education proposal.</a>",
-        "thumbnail": "thinktank.jpg",
         "images": ['thinktank.jpg'],
         "languages": ['HTML', 'CSS', 'JS', 'jQuery']
     },
     {
         "id": 14,
         "title": "TU20 Cup Event Website",
-        "width": 640,
         "description": "Event website for the TechUnderTwenty Cup, featuring a schedule and response form.",
-        "thumbnail": "cup.jpg",
         "images": ['cup.jpg'],
         "languages": ['HTML', 'CSS', 'JS', 'jQuery']
     }
-
 ]
 
-
-
-
 $(document).ready(function() {
-
     // move slightly up while fading into view
     function fadeInAnimation(elem) {
         elem.css("margin-top", "+=30px");
@@ -165,30 +130,16 @@ $(document).ready(function() {
 
     fadeIn();
 
-    for (var i = 0; i < projects.length; i++) {
-        project = projects[i];
-
-        toAppend = '<div class="mdl-cell mdl-cell--4-col"><div project="' + project['id'] + '" class="mdl-card-square mdl-card">';
-        toAppend += '<img src="img/thumbnails/' + project['thumbnail'] + '" class="projectImg">';
-        toAppend += '<center><figcaption>' + project['title'] + '</p></center>';
-        toAppend += '</div></div>';
-
-        $('.mdl-container').append(toAppend);
-    }
-
     $('.mdl-card').on('click', function(e) {
         document.location.href = '#modal';
         num = $(e.currentTarget).attr('project');
         projectClicked = projects[num];
-
         $('.remodal').empty();
-
         toAppend = '<center><button data-remodal-action="close" class="remodal-close"></button>';
         toAppend += '<h1 style="font-size: 45px; font-family: Equal;">' + projectClicked['title'] + '</h1>';
 
         if (projectClicked.images.length != 0) {
-            toAppend += ' <div class="csslider1 no-autoplay" style="max-width:' + projectClicked['width'] + 'px;">';
-
+            toAppend += ' <div class="csslider1 no-autoplay" style="max-width: 640px;">';
             for (var i = 0; i < projectClicked.images.length; i++) {
                 toAppend += '<input name="cs_anchor1" id="cs_slide1_' + i + '" type="radio" class="cs_anchor slide"';
                 if (i == 0) {
@@ -196,42 +147,31 @@ $(document).ready(function() {
                 }
                 toAppend += '>';
             }
-
             toAppend += '<ul>';
-
             for (var i = 0; i < projectClicked.images.length; i++) {
                 if (i == 0) {
                     toAppend += '<li class="cs_skeleton"><img src="cssslider_files/csss_images1/' + projectClicked.images[i] + '" style="width: 100%;"></li>';
                 }
                 toAppend += '<li class="num' + i + ' img slide"> <img src="cssslider_files/csss_images1/' + projectClicked.images[i] + '" alt="' + projectClicked.images[i].slice(0, -4) + '" title="' + projectClicked.images[i].slice(0, -4) + '"></li>';
             }
-
             toAppend += '</ul><div class="cs_arrowprev">';
-
             for (var i = 0; i < projectClicked.images.length; i++) {
                 toAppend += '<label class="num' + i + '" for="cs_slide1_' + i + '"><span><i></i><b></b></span></label>';
             }
             toAppend += '</div><div class="cs_arrownext">';
-
             for (var i = 0; i < projectClicked.images.length; i++) {
                 toAppend += '<label class="num' + i + '" for="cs_slide1_' + i + '"><span><i></i><b></b></span></label>';
             }
-
             toAppend += '</div></div>'
         }
 
         toAppend += '</center><div class="langs">Languages: ' + projectClicked.languages[0];
 
         for (var i = 1; i < projectClicked.languages.length; i++) {
-          toAppend +=  ', ' + projectClicked.languages[i];
+            toAppend += ', ' + projectClicked.languages[i];
         }
-        toAppend += '</div><br>';
-
-                toAppend += '<p class="projectDesc">' + projectClicked['description'] + '</p>';
-
+        toAppend += '</div><br><p class="projectDesc">' + projectClicked['description'] + '</p>';
         toAppend += '<button data-remodal-action="confirm" class="remodal-confirm">O K</button>';
         $('.remodal').append(toAppend);
-
     });
-
 });
