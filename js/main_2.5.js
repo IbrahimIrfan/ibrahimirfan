cf9fbce45eb63e159589ed68847f245243f69faa
IbrahimIrfan <1ibrahimirfan@gmail.com>
2017-12-08 14:36:52 -0500

    Updated url

--- js/main_2.5.js
+++ js/main_2.5.js
@@ -1,7 +1,7 @@
 var projects = [{
 	id: 0,
 	title: "Point-To-Speech",
-	description: "MasseyHacks 3 - 3rd Place Finalist.<br><br>Created a device to help blind people read regular pages of text using simple, intuitive hand gestures. A Raspberry Pi with a connected camera was attached to PVC pipe. The paper was placed below the camera, with a LeapMotion attached with a 3D printed stand in front of it.<br><br> Once the user places the paper in position, the camera takes a picture and sends it to a server. Then, the accompanying web application uses <a href='http://tesseract.projectnaptha.com/' target='_blank'>TesseractJS</a> to parse the text.<br><br> Using calculated offset and conversion factors, the coordinates returned by the program were mapped for the LeapMotion - so that the word had a location in physical space.<br><br> Then, when the user points to a word on the page, the web app will say the word using <a href='https://responsivevoice.org/' target='_blank'>ResponsiveVoiceJS</a>. ",
+	description: "<a href='https://devpost.com/software/point-to-speech-rvsa0y'>MasseyHacks 3 - 3rd Place Finalist.</a><br><br>Created a device to help blind people read regular pages of text using simple, intuitive hand gestures. A Raspberry Pi with a connected camera was attached to PVC pipe. The paper was placed below the camera, with a LeapMotion attached with a 3D printed stand in front of it.<br><br> Once the user places the paper in position, the camera takes a picture and sends it to a server. Then, the accompanying web application uses <a href='http://tesseract.projectnaptha.com/' target='_blank'>TesseractJS</a> to parse the text.<br><br> Using calculated offset and conversion factors, the coordinates returned by the program were mapped for the LeapMotion - so that the word had a location in physical space.<br><br> Then, when the user points to a word on the page, the web app will say the word using <a href='https://responsivevoice.org/' target='_blank'>ResponsiveVoiceJS</a>. ",
 	images: ["MH1.jpg", "MH2.jpg", "MH3.jpg", "MH4.jpg"],
 	type: "hack",
 	thumbnail: "MH1.jpg",
@@ -9,7 +9,7 @@ var projects = [{
 }, {
 	id: 1,
 	title: "DriveBud",
-	description: "For my group's SHAD Valley project, we decided to address the yearly theme of reducing environmental footprint with DriveBud.<br><br>Partnering with my employer Geotab, a large fleet management telematics company, we used their GO7 Device to track vehicle data. This device would plug into a customers car and send statistics to the DriveBud app. The app presents the data to the user in a fun, friendly way and makes eco-friendly driving appealing.<br><br>Our project team will be advancing to the National stage in October.<br><br><iframe width='560' height='315' src='https://www.youtube.com/embed/ctLM8qvnsoc' frameborder='0' allowfullscreen></iframe>",
+	description: "<a href='https://github.com/IbrahimIrfan/drivebudapp'>GitHub</a><br><br>For my group's SHAD Valley project, we decided to address the yearly theme of reducing environmental footprint with DriveBud.<br><br>Partnering with my employer Geotab, a large fleet management telematics company, we used their GO7 Device to track vehicle data. This device would plug into a customers car and send statistics to the DriveBud app. The app presents the data to the user in a fun, friendly way and makes eco-friendly driving appealing.<br><br>Our project team will be advancing to the National stage in October.<br><br><iframe width='560' height='315' src='https://www.youtube.com/embed/ctLM8qvnsoc' frameborder='0' allowfullscreen></iframe>",
 	images: [],
 	type: "personal",
 	thumbnail: "drive.png",
@@ -17,7 +17,7 @@ var projects = [{
 }, {
 	id: 2,
 	title: "DECA Exam Parser",
-	description: "<a href='https://github.com/IbrahimIrfan/DECA-exam-parser'>Exam Parser</a> &middot; <a href='https://github.com/IbrahimIrfan/DECA-Web-App'>Web Application</a> Created a program to parse poorly formatted PDF practice exams and output self marking online forms for my high school's DECA club (a case competition). <br><br> I also implemented a registration and login system with PHP and SQL for members to take those exams under their name on our website; allowing the executives to monitor their progress.<br><br> Other functionality included executive-only announcement posting (along with email notifications once an announcement is posted), as well as other resources for each event.",
+	description: "<a href='https://github.com/IbrahimIrfan/DECA-exam-parser'>Exam Parser</a> &middot; <a href='https://github.com/IbrahimIrfan/DECA-Web-App'>Web Application</a> <br><br> Created a program to parse poorly formatted PDF practice exams and output self marking online forms for my high school's DECA club (a case competition). <br><br> I also implemented a registration and login system with PHP and SQL for members to take those exams under their name on our website; allowing the executives to monitor their progress.<br><br> Other functionality included executive-only announcement posting (along with email notifications once an announcement is posted), as well as other resources for each event.",
 	images: ["DECA2.jpg", "DECA3.jpg", "DECA4.jpg", "DECA5.jpg", "DECA6.jpg", "DECA7.jpg", "DECA8.jpg", "DECA9.jpg"],
 	type: "personal",
 	thumbnail: "DECA.png",
@@ -25,7 +25,7 @@ var projects = [{
 },  {
 	id: 3,
 	title: "University of Guelph Exam Network",
-	description: "During my internship as a Software Developer for Viceroy Media, I worked on an exam network web application for the University of Guelph.<br><br>It contained a file upload system where UofG students could upload study material for specific courses for others to use. The application also included a login system with Facebook API integration, a public forum, and course/professor ratings.<br><br> My specific role was to develop the functionality for the file system (uploading, managing, and viewing the uploaded material) and login system - working with JS/jQuery and API requests to do so.",
+	description: "<a href='http://uog.viceroymedia.net/'>View</a> <br><br>During my internship as a Software Developer for Viceroy Media, I worked on an exam network web application for the University of Guelph.<br><br>It contained a file upload system where UofG students could upload study material for specific courses for others to use. The application also included a login system with Facebook API integration, a public forum, and course/professor ratings.<br><br> My specific role was to develop the functionality for the file system (uploading, managing, and viewing the uploaded material) and login system - working with JS/jQuery and API requests to do so.",
 	images: ["uog.jpg", "uog2.jpg", "uog3.jpg", "uog4.jpg", "uog5.jpg"],
 	type: "work",
 	thumbnail: "uog.jpg",
@@ -33,7 +33,8 @@ var projects = [{
 }, {
 	id: 4,
 	title: "BridgeDay Conference Registration System",
-	description: 'At my high school, a full day conference is hosted every year where incoming students come to the shool for a day of talks, workshops, and introductions.<br><br>When 350 grade 8 students are coming to your school from many different locations, you need someone to take on the hectic registration process - that was my role.<br><br>I decided to make a conference website, showcasing the plan of the day and with important information for students and teacher supervisors. They were also able to register directly from the website - which allowed us to send various schools a simple link instead of a long google form.<br><br>Students had a choice of what workshops they wished to attend, with their preference indicated on the registration form. As workshops were filled on a first come first serve basis, I implemented a live "Workshop Status" display, indicating the spaces currently available in each workshop. Along with this, I created a program which would generate nametags for every student attending the conference.<br><br> Overall, this was a great opportunity for me to experience how to properly manage a registration system and how to correctly deal with data. In addition - the conference turned out very successful, with no major hiccups.',
+	description: '<a href="https://github.com/IbrahimIrfan/bridgeday">GitHub</a> <br><br>Created a conference registration system for over 300 students. As workshops were filled on a first come first serve basis, I implemented a live "Workshop Status" display, indicating the spaces currently available in each workshop. Additionally, I created a program which would generate nametags for every student attending the conference.<br><br>',
+
 	images: ["bridgeday.jpg", "bridgeday2.jpg", "bridgeday3.jpg", "bridgeday4.jpg", "nametags.jpg"],
 	type: "personal",
 	thumbnail: "bridgeday.jpg",
@@ -41,7 +42,7 @@ var projects = [{
 }, {
 	id: 5,
 	title: "Self-Aiming Cannon",
-	description: "Self-aiming cannon that automatically calculates trajectories towards a target built using Python, Go, and a RaspberryPi. Recognizes a face with SkyBiometry API after taking a picture and aims towards it. Includes remote control with keyboard input through a socket connection.<br><br><center><img src='img/source.gif' width='80%'></center>",
+	description: "<a href='https://github.com/edwinzhng/automatic-cannon'>GitHub</a><br><br>Self-aiming cannon that automatically calculates trajectories towards a target built using Python, Go, and a RaspberryPi. Recognizes a face with SkyBiometry API after taking a picture and aims towards it. Includes remote control with keyboard input through a socket connection.<br><br><center><img src='img/source.gif' width='80%'></center>",
 	images: ["cannon.jpg", "cannon2.jpg"],
 	type: "school",
 	thumbnail: "cannon.jpg",
@@ -66,7 +67,7 @@ var projects = [{
 }, {
 	id: 8,
 	title: "Hack the Fridge",
-	description: "Hack the North 2017 - Honourable Mention<br><br>Our goal for Hack the North was to find a way to reduce food waste in an efficient and organized process. We set out to create a smart fridge to accomplish this goal and by implementing Watson's Visual Recognition API and various other technologies.<br><br>Hack the Fridge is a smart fridge which takes pictures of items put into it, keeps track of their expiry date, and gives recipe ideas in an effort to reduce food waste. The fridge takes a picture of each food item as it is placed in the fridge and compiles a list of possible recipes based on that item and the items already in the fridge. It also records the date each food item was added and keeps track of their expiry.<br><br>The fridge is made entirely from materials found at Hack the North, including all the food items we used. We took pictures using a Raspberry Pi and camera with Python and sent them to a Node.js server on AWS. Each food item was stored as an object using MongoDB, the data was then presented on an iOS app.",
+	description: "<a href='https://github.com/derrekchow/hackthefridge'>GitHub</a><br><br><a href='https://devpost.com/software/hack-the-fridge'>Hack the North 2017 - Honourable Mention</a><br><br>Hack the Fridge is a smart fridge which takes a picture of each food item as it is placed in the fridge and compiles a list of possible recipes based on that item and the items already in the fridge. It also records the date each food was added and keeps track of their expiry.<br><br> We took pictures using a Raspberry Pi camera with Python and sent them to a Node.js server on AWS. Each food item was stored as an object using MongoDB, the data was then presented on an iOS app.",
 	images: ["htf.jpg", "htf2.jpg", "htf3.jpg", "htf4.jpg", "htf5.jpg", "htf6.jpg"],
 	type: "hack",
 	thumbnail: "htf.jpg",
@@ -74,7 +75,7 @@ var projects = [{
 }, {
 	id: 9,
 	title: "2 Player Chess Game",
-	description: "My final project for Grade 11 Computer Science - a two-player Chess game built with Turing. One of the most difficult yet satisfying projects I have taken on. The final product was nearly fully functional - all but checkmate detection was working.",
+	description: "<a href='https://github.com/IbrahimIrfan/chess'>GitHub</a><br><br>My final project for Grade 11 Computer Science - a two-player Chess game built with Turing. One of the most difficult yet satisfying projects I have taken on. The final product was nearly fully functional.",
 	images: ["chess.jpg", "chess2.jpg", "chess3.jpg", "chess4.jpg"],
 	type: "school",
 	thumbnail: "chess.jpg",
@@ -82,7 +83,7 @@ var projects = [{
 }, {
 	id: 10,
 	title: "Geotab HR Automation System",
-	description: "For the summer of 2017, I was a Jr. Software Developer Intern for the HR Department at Geotab. The goal of this project was to increase the efficiency of Geotab's HR Department by automating many tedious, time-consuming tasks. I created a collection of Docs, Forms, and Spreadsheets using Google Apps Script to automate many of the tasks, summarized below. As much of this is internal data, I cannot go into detail or include screenshots of the project itself for security purposes.<br><br>•  Job Applications<br>•  Interviews<br>•  Handling Employee Files<br>•  Scheduling Meetings<br>•  Emails<br>•  Internal Reporting<br>•  Resume Parsing<br>•  Workshop Registration<br>•  Performance Reviews<br>•  File Revision History",
+	description: "For the summer of 2017, I was a Jr. Software Developer Intern for the HR Department at Geotab. My job was to increase the efficiency of Geotab's HR Department by automating many tedious, time-consuming tasks. I created a collection of Docs, Forms, and Spreadsheets using Google Apps Script to automate many of the tasks, summarized below. <br><br>•  Job Applications<br>•  Interviews<br>•  Handling Employee Files<br>•  Scheduling Meetings<br>•  Emails<br>•  Internal Reporting<br>•  Resume Parsing<br>•  Workshop Registration<br>•  Performance Reviews<br>•  File Revision History",
 	images: ["geotab.png"],
 	type: "work",
 	thumbnail: "geotab.png",
@@ -90,7 +91,7 @@ var projects = [{
 }, {
 	id: 11,
 	title: "ITEE Group Canada",
-	description: "Freelance develoment work for a small local consulting firm, ITEE Group Canada. Created an infomational website dislaying information with an email contact form.",
+	description: "<a href='http://iteegroupcanada.com/index.html'>View</a> &middot; <a href='https://github.com/IbrahimIrfan/itee'>GitHub</a>Freelance develoment work for a small local consulting firm, ITEE Group Canada. Created an infomational website dislaying information with an email contact form.",
 	images: ["itee.png", "itee2.png"],
 	type: "work",
 	thumbnail: "thinktank.jpg",
