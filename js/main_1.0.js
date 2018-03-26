666676e055b70d26cc0ba309721ed0e738ee72a0
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-03-26 19:01:04 -0400

    fixed projectClickedon page load

--- js/main_1.0.js
+++ js/main_1.0.js
@@ -1,23 +1,23 @@
 var app = angular.module("portfolio", []);
 
 app.controller("projects", function($scope) {
-    $scope.projects = projects;
+	$scope.projects = projects;
 	$scope.projectClicked = function(id){
 		console.log("this");
 		$scope.currentProject = $scope.projects[id];
-        document.location.href = "#modal";
+		document.location.href = "#modal";
 		window.history.pushState("index", "Project", "/?project=" + id + "#modal");
 	}
 
-    $scope.getParameterByName = function (name, url) {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
-    }
+	$scope.getParameterByName = function (name, url) {
+		if (!url) url = window.location.href;
+		name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
 
 	$scope.arrows = ["img/icons/whiteArrow.png", "img/icons/greenArrow.png"]
 	// todo: make arrow fade out/in
@@ -26,13 +26,13 @@ app.controller("projects", function($scope) {
 		// make arrow click go to ".content" on ng-click
 	}
 
-    $scope.queryNum = $scope.getParameterByName("project");
-    $scope.queryNum2 = $scope.getParameterByName("p");
-    if ($scope.queryNum != null && window.location.hash.substr(1) == "modal") {
-        $scope.projectClicked(parseInt($scope.queryNum));
-    } else if ($scope.queryNum2 != null && window.location.hash.substr(1) == "modal") {
-       	$scope.projectClicked(parseInt($scope.queryNum2));
-    }
+	$scope.queryNum = $scope.getParameterByName("project");
+	$scope.queryNum2 = $scope.getParameterByName("p");
+	if ($scope.queryNum != null && window.location.hash.substr(1) == "modal") {
+		$scope.projectClicked(parseInt($scope.queryNum));
+	} else if ($scope.queryNum2 != null && window.location.hash.substr(1) == "modal") {
+		$scope.projectClicked(parseInt($scope.queryNum2));
+	}
 
 });
 
@@ -41,3 +41,21 @@ app.filter("trustThis", ["$sce", function($sce){
 		return $sce.trustAsHtml(text);
 	};
 }]);
+
+app.directive('scrollOnClick', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, $elm, attrs) {
+			var idToScroll = attrs.href;
+			$elm.on('click', function() {
+				var $target;
+				if (idToScroll) {
+					$target = $(idToScroll);
+				} else {
+					$target = $elm;
+				}
+				$("body").animate({scrollTop: $target.offset().top}, "slow");
+			});
+		}
+	}
+});
