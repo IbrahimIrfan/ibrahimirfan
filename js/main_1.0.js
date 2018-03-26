c163c3a36140ac15f2e14162a182d8dcd2bd4b6f
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-03-26 17:01:01 -0400

    fixed projectClickedon page load

--- js/main_1.0.js
+++ js/main_1.0.js
@@ -3,6 +3,7 @@ var app = angular.module("portfolio", []);
 app.controller("projects", function($scope) {
     $scope.projects = projects;
 	$scope.projectClicked = function(id){
+		console.log("this");
 		$scope.currentProject = $scope.projects[id];
         document.location.href = "#modal";
 		window.history.pushState("index", "Project", "/?project=" + id + "#modal");
