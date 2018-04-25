f87ed14caa6d0973d8cd6f6ed50570eca0144865
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-04-24 22:47:13 -0400

    Merged gh-pages into master

---
+++ js/main_1.0.js
@@ -0,0 +1,31 @@
+particlesJS.load('particles-js', "js/particles.json");
+
+var app = angular.module("portfolio", ['ngAnimate']);
+
+app.controller("main", function($scope) {
+	// handle the loading screen fade
+	$scope.loading = false;
+	angular.element(function(){
+		$scope.$apply(function(){
+			$scope.loading = true;
+		});
+	});
+
+	$scope.scrollTo = function() {
+		console.log("Called");
+		scroller = document.getElementById("scroll");
+		if (scroller.getBoundingClientRect().top > 0){
+			setTimeout(function() {
+				window.scrollTo(0,window.scrollY+25);
+				$scope.scrollTo();
+			}, 15);
+		}
+	}
+
+	$scope.projects = projects;
+	$scope.projectClicked = function(id){
+		document.location.href = "projects.html?p=" + id;
+	}
+
+	$scope.arrows = ["img/icons/whiteArrow.png", "img/icons/greenArrow.png"]
+});
