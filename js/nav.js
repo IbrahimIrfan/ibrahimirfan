aac68c0648ad12c9161cb055a480a5c1d6ed18f9
Ibrahim Irfan <1ibrahimirfan@gmail.com>
2020-07-08 00:38:13 -0400

    oops

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function () {
+    populate_nav()
+});
+
+function populate_nav() {
+    var body = document.body;
+    nav = "<ul class='nav justify-content-center'>" +
+        "<li class='nav-item'>" +
+        "    <a class='nav-link active' href='/'>Home</a>" +
+        "</li>" +
+        "<li class='nav-item'>" +
+        "    <a class='nav-link' href='/writing'>Writing</a>" +
+        "</li>" +
+        "<li class='nav-item'>" +
+        "    <a class='nav-link' href='/contact'>Contact</a>" +
+        "</li>" +
+        "</ul>"
+    body.innerHTML = nav + body.innerHTML;
+}
